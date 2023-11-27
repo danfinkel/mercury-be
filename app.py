9177fcfd9423fb4f2cf97ed0c841579e39fbf532
Dan Finkel <dan.finkel@gmail.com>
2023-11-27 16:13:45 -0500

    test

--- app.py
+++ app.py
@@ -12,15 +12,19 @@ def hello_world():
 def get_index():   
     def simplefcn():
         import time
-        # yield str({"user": "dan", "content": "Hello this is a test"})
-        yield "Hello this is a test" + "\n"
+        yield str({"user": "dan", "content": "Hello this is a test"}) + "\n"
+        # yield "Hello this is a test" + "\n"
         time.sleep(2)
-        yield "AI is not wired up yet" + "\n"
+        # yield "AI is not wired up yet" + "\n"
+        yield str({"user": "dan", "content": "AI is not wired up yet"}) + "\n"
         time.sleep(2)
-        yield "So instead you are getting this automatic response" + "\n"
+        # yield "So instead you are getting this automatic response" + "\n"
+        yield str({"So instead you are getting this automatic response"}) + "\n"
         time.sleep(2)
-        yield "Perhaps next time..." + "\n"
+        # yield "Perhaps next time..." + "\n"
+        yield str({"Perhaps next time..."}) + "\n"
         time.sleep(2)
-        yield "OK goodbye" + "\n"
+        # yield "OK goodbye" + "\n"
+        yield str({"OK goodbye"}) + "\n"
     
     return simplefcn()
