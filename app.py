b31f5187659aadc93d3fad7af8a8bcbdc1c115dc
Dan Finkel <dan.finkel@gmail.com>
2023-11-27 16:05:59 -0500

    test

--- app.py
+++ app.py
@@ -13,14 +13,14 @@ def get_index():
     def simplefcn():
         import time
         # yield str({"user": "dan", "content": "Hello this is a test"})
-        yield "Hello this is a test"
+        yield "Hello this is a test" + "\n"
         time.sleep(2)
-        yield "AI is not wired up yet"
+        yield "AI is not wired up yet" + "\n"
         time.sleep(2)
-        yield "So instead you are getting this automatic response"
+        yield "So instead you are getting this automatic response" + "\n"
         time.sleep(2)
-        yield "Perhaps next time..."
+        yield "Perhaps next time..." + "\n"
         time.sleep(2)
-        yield "OK goodbye"
+        yield "OK goodbye" + "\n"
     
     return simplefcn()
